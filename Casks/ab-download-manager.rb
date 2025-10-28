1a00bf99f30576afde789c5d8581f9324b660c34
CoZo <2606767372@qq.com>
2025-10-28 15:10:29 +0800

    Update ab-download-manager.rb

--- Casks/ab-download-manager.rb
+++ Casks/ab-download-manager.rb
@@ -1,8 +1,6 @@
 cask "ab-download-manager" do
-  version "1.7.0"
-
   arch arm: "arm64", intel: "x64"
-
+  version "1.7.0"
   sha256 arm:   "43b94b1f76f7b0604376676edddf3ee68bbdca9dd3256fc8f49513167fd55fd6",
          intel: "6577814840f6c14e8f71a0e2e02ff354596b65a951568ae064715cc75371d545"
 
